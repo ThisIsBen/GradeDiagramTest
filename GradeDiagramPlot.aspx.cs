3951031b1b736c60b26644f12ad369032e372c88
t5i0m7 <p840507@gmail.com>
2017-08-13 18:52:57 +0800

    Revert "C# script advise"
    This reverts commit 0540b6b0aa41772f71f844eae92d0f7ddf621d15.

--- GradeDiagramPlot.aspx.cs
+++ GradeDiagramPlot.aspx.cs
@@ -7,8 +7,6 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
 using System.Text;
-using System.Diagnostics;
-
 
 namespace GradeDiagramTest
 {
@@ -16,17 +14,15 @@ namespace GradeDiagramTest
     {
         // Student Scroe and Question from Database
 
-        public string []FirstColDefault = {"學生","Question1總分"};
+        public string []FirstColDefault = {"總分"};
         //
        
         List<ScoreAnalysisM> ScoreAnalysisList = new List<ScoreAnalysisM>();
         private int table_rows;
         private int table_cols;
         private int []QuestionAvg;
-        private int MemberQuestionNum;
+        private int QuestionNum;
         private int studentNum;
-        private int currentQuestion=1;
-        private int data_implicit_num = 1;
         private List<string> AddRowsName = new List<string>();
         private List<string> AddColsName = new List<string>();
         private TableCell tc_temp;
@@ -45,40 +41,38 @@ namespace GradeDiagramTest
             //Get the retrieved data from each row of the retrieved data table.
             foreach (DataRow dr in dt.Rows)
             {
-                ScoreAnalysisM log = new ScoreAnalysisM(dr.Field<string>("StuCouHWDe_ID"), dr.Field<string>("Grade"));
+                ScoreAnalysisM log = new ScoreAnalysisM();
+
+                log.StuCouHWDe_ID = dr.Field<string>("StuCouHWDe_ID");
+                log.Grade = dr.Field<string>("Grade").Split(',').ToList<string>();
+                log.QuestionNum = Convert.ToInt16(log.Grade[1]);
+                log.Grade.RemoveAt(1);
                 ScoreAnalysisList.Add(log);
 
             }
 
 
-            ////Initalize the member variable
-            MemberVariableSet(currentQuestion);
+            //Initalize the member variable
 
-            
+            table_cols = FirstColDefault.Length + ScoreAnalysisList[0].Grade.Count-1 ;
+            table_rows = ScoreAnalysisList.Count;
+            studentNum = table_rows;
+            QuestionNum = ScoreAnalysisList[0].Grade.Count - 1;
+            // Add a row named Avg
+            AddRow("Avg");
 
             GenerateTable(table_cols, table_rows);
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < MemberQuestionNum; i++)
+            StringBuilder sb = new StringBuilder();  
+            for (int i = 0; i < QuestionNum;i++ )
             {
                 string temp2;
-                temp2 = "['第" + (i + 1) + "題'," + QuestionAvg[i] + "],";
-                sb.Append(temp2);
-
+                temp2 = "['第" + (i+1) + "題'," + QuestionAvg[i] + "],";
+                 sb.Append(temp2);
+                
             }
-            chart.InnerHtml = "<script>var chart = c3.generate({bindto: '#chart',data: {columns: [" + sb.ToString() + "],type : 'pie'}});</script>";
+            chart.InnerHtml="<script>var chart = c3.generate({bindto: '#chart',data: {columns: ["+sb.ToString()+"],type : 'pie'}});</script>";
             
-        }
-
-        private void MemberVariableSet(int Question_num)
-        {
-            currentQuestion = Question_num;
-            table_cols = ScoreAnalysisList[0].Grade[currentQuestion].Length - data_implicit_num;
-            table_rows = ScoreAnalysisList.Count;
-            studentNum = table_rows;
-            MemberQuestionNum = ScoreAnalysisList[0].MemberQuestionNum[currentQuestion];
-            //// Add a row named Avg
-            AddRow("Avg");
         }
 
         protected void DownLoadToExl_Click(object sender, EventArgs e)
@@ -138,7 +132,7 @@ namespace GradeDiagramTest
             for (int i = 0; i <= rowsCount; i++)
             {
                 TableRow row = new TableRow();
-                for (int j = 0; j < colsCount; j++)
+                for (int j = 0; j <= colsCount; j++)
                 {
                     TableCell cell = new TableCell();
                     cell.ID = "TextBoxRow_" + i + "Col_" + j;
@@ -150,16 +144,17 @@ namespace GradeDiagramTest
 
             for (int i = 1; i <= rowsCount - AddRowsName.Count; i++)
             {
-                for (int j = ScoreAnalysisList[i - 1].QueIndex_start; j <ScoreAnalysisList[i - 1].Grade[currentQuestion].Length; j++)
-                    InsertTableStr(i, j - data_implicit_num, ScoreAnalysisList[i - 1].Grade[currentQuestion][j]);
+                int StudentDataLength = ScoreAnalysisList[i - 1].QuestionNum + FirstColDefault.Length;
+                for (int j = 1; j <=StudentDataLength; j++)
+                    InsertTableStr(i, j, ScoreAnalysisList[i - 1].Grade[j - 1]);
            }
             
             // Row 0 set
-            for (int i = 0; i < FirstColDefault.Length; i++)
-                InsertTableStr(0, i, FirstColDefault[i]);
+            for (int i = 1; i <= FirstColDefault.Length; i++)
+                InsertTableStr(0, i, FirstColDefault[i - 1]);
             
-            for (int i = 1; i <=MemberQuestionNum; i++)
-                InsertTableStr(0, i+FirstColDefault.Length-1, "第" + (i) + "題");
+            for (int i = FirstColDefault.Length + 1; i <= colsCount; i++)
+                InsertTableStr(0, i, "第" + (i - FirstColDefault.Length) + "題");
 
             // Column 0 set
             for (int i = 1; i <= studentNum; i++)
@@ -167,22 +162,18 @@ namespace GradeDiagramTest
             
             for (int i = studentNum + 1; i <= rowsCount; i++)
                 InsertTableStr(i, 0, AddRowsName[i - rowsCount]);
-
-            // Add total column
-           for (int row = 1; row <= studentNum; row++)
-               InsertTableStr(row, 1, ScoreAnalysisList[row - 1].Grade[currentQuestion][1]);
             
-                
-            //Average Calculage
-            QuestionAvg = new int[MemberQuestionNum];
-            for (int index = 0; index <MemberQuestionNum; index++)
+
+            // Average Calculage
+            QuestionAvg = new int[QuestionNum];
+            for (int j = FirstColDefault.Length+1; j <=colsCount; j++)
             {
                 int sum = 0;
-                for (int row = 1; row <= studentNum; row++)
-                    sum = Convert.ToInt16(GetTableStr(row, index+FirstColDefault.Length)) + sum;
-                sum=sum/studentNum;
-                InsertTableStr(rowsCount, index + FirstColDefault.Length, sum.ToString());
-                QuestionAvg[index] = sum;
+                for (int i = 1; i < rowsCount; i++)
+                    sum = Convert.ToInt16(GetTableStr(i, j)) + sum;
+                sum=sum/(rowsCount-1);
+                InsertTableStr(rowsCount, j, sum.ToString());
+                QuestionAvg[j-FirstColDefault.Length - 1] = sum;
             }
             
         }
