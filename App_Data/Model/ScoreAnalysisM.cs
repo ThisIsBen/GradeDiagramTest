0540b6b0aa41772f71f844eae92d0f7ddf621d15
t5i0m7 <p840507@gmail.com>
2017-08-13 18:52:43 +0800

    C# script advise
    ScoreAnalyzeM class 修改以符合資料庫需求
    將產生table的sciprt 做修改方便tab的使用

--- App_Data/Model/ScoreAnalysisM.cs
+++ App_Data/Model/ScoreAnalysisM.cs
@@ -10,6 +10,37 @@ using System.Web;
 public class ScoreAnalysisM
 {
     public string StuCouHWDe_ID;
-    public List<string> Grade;
+    public List<string[]> Grade=new List<string[]>();
     public int QuestionNum;
+    public int[] MemberQuestionNum;
+    public int QueIndex_start=3;
+
+    public ScoreAnalysisM(string id,string grade_str)
+    {
+        string[] temp_str_arr = MemberQueSpilt(grade_str);
+        StuCouHWDe_ID = id;
+        for (int i = 0; i < QuestionNum; i++)
+        {
+            MemberQueGradeSpilt(temp_str_arr[i], i);
+        }
+
+    }
+
+    private string[] MemberQueSpilt(string grade_str)
+    {
+        string[] temp_str_arr = grade_str.Split(':');
+        QuestionNum = temp_str_arr.Length;
+        MemberQuestionNum = new int[QuestionNum];
+        return temp_str_arr;
+    }
+
+    private string[] MemberQueGradeSpilt(string grade_member_str,int questionNum)
+    {
+        string[] temp_str_arr = grade_member_str.Split(',');
+        MemberQuestionNum[questionNum] = temp_str_arr.Length - QueIndex_start;
+        Grade.Add(temp_str_arr);
+        return temp_str_arr;
+    }
+
+
 }
